33673c27eb6fec0bf05441270084e87afad42c7b
unknown <e0968931@u.nus.edu>
2023-06-25 10:03:06 +0800

    LoginScreen is working fine

--- src/view/authentication/LoginScreen.js
+++ src/view/authentication/LoginScreen.js
@@ -32,105 +32,144 @@ export default function LoginScreen() {
     event.preventDefault();
   };
 
+  const handleClickLogin = () =>
+    handleLogin({ email: email, password: password }).then((msg) => {
+      setLoginMessage(msg);
+      if (loginMessage !== "Success") {
+        setOpen(true);
+      }
+    });
+
   return (
-    <Container
-      maxWidth="xs"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        backgroundColor: "",
-        justifyBackground: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Box
+    <>
+      <Container
+        maxWidth="xs"
         sx={{
           display: "flex",
           flexDirection: "column",
-          height: "35%",
-          width: "100%",
-          bgcolor: "#eaeaea",
-          padding: "16px",
+          height: "100vh",
+          justifyBackground: "center",
+          justifyContent: "center",
         }}
       >
-        <div className="Email-input">
-          <TextField
-            id="outlined-start-adornment"
-            error={email === ""}
-            helperText="Please enter your email."
-            sx={{
-              m: 1,
-              width: "40ch",
-              position: "relative",
-              left: "5%",
-              top: "20%",
-            }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">Email:</InputAdornment>
-              ),
-            }}
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-          />
-        </div>
-        <div className="Password-input">
-          <FormControl
-            sx={{
-              m: 1,
-              width: "40ch",
-              position: "relative",
-              left: "5%",
-              top: "20%",
-            }}
-            error={password === ""}
-            variant="outlined"
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          >
-            <InputLabel htmlFor="outlined-adornment-password">
-              Enter your Password
-            </InputLabel>
-            <OutlinedInput
-              id="outlined-adornment-password"
-              type={showPassword ? "text" : "password"}
-              endAdornment={
-                <InputAdornment position="end">
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              }
-              label="Password"
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            height: "35%",
+            width: "100%",
+            bgcolor: "#eaeaea",
+            padding: "16px",
+          }}
+        >
+          <div className="Email-input">
+            <TextField
+              id="outlined-start-adornment"
+              error={email === ""}
+              helperText="Please enter your email."
+              sx={{
+                m: 1,
+                width: "80%",
+                position: "relative",
+                left: "8%",
+                top: "20%",
+              }}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">Email:</InputAdornment>
+                ),
+              }}
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
             />
-          </FormControl>
-        </div>
-        <div className="LoginButton">
-          <Button
-            variant="contained"
-            sx={{ m: 1, position: "relative", left: "75%", top: "40%" }}
-            onClick={() => setLoginMessage(handleLogin({ email, password }))}
+          </div>
+          <div className="Password-input">
+            <FormControl
+              sx={{
+                m: 1,
+                width: "80%",
+                position: "relative",
+                left: "8%",
+                top: "20%",
+              }}
+              error={password === ""}
+              variant="outlined"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Enter your Password
+              </InputLabel>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+                label="Password"
+              />
+            </FormControl>
+          </div>
+          <div className="LoginButton">
+            <Button
+              variant="contained"
+              sx={{ m: 1, position: "relative", left: "75%", top: "40%" }}
+              onClick={handleClickLogin}
+            >
+              Login
+            </Button>
+          </div>
+          <div className="Links">
+            <div>
+              <Link
+                sx={{ position: "relative", left: "17%", top: "40%" }}
+                href="#"
+              >
+                Don't have an account? Sign up!
+              </Link>
+            </div>
+            <div>
+              <Link
+                sx={{ position: "relative", left: "30%", top: "40%" }}
+                href="#"
+              >
+                Forgot password
+              </Link>
+            </div>
+          </div>
+        </Box>
+        <Collapse in={open}>
+          <Alert
+            severity="warning"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setOpen(false);
+                }}
+              >
+                <Close />
+              </IconButton>
+            }
           >
-            Login
-          </Button>
-        </div>
-        <div className="Links">
-          <div>
-            <Link href="#">Don't have an account? Sign up!</Link>
-          </div>
-          <div>
-            <Link href="#">Forgot password</Link>
-          </div>
-        </div>
-      </Box>
-    </Container>
+            <AlertTitle>Warning</AlertTitle>
+            {loginMessage}
+          </Alert>
+        </Collapse>
+      </Container>
+    </>
   );
 }
