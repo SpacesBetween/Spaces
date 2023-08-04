6a87a89f09b6260f3aa10581c6c272d4d3f9a073
unknown <e0968931@u.nus.edu>
2023-08-04 13:08:47 +0800

    Positioned sign out button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "./NavButton.js";
+import { signOut } from "../model/auth/auth.js";
 import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
 
+  // functions
+  const logOff = () => {
+    signOut();
+  };
+
   const handleClick = () => setClick(!click);
+
   const closeMobileMenu = () => setClick(false);
 
   return (
@@ -26,32 +33,49 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="roomsearch" className="nav-links" onClick={closeMobileMenu}>
+              <Link
+                to="roomsearch"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 BOOKINGS
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="eventsearch" className="nav-links" onClick={closeMobileMenu}>
+              <Link
+                to="eventsearch"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 EVENTS
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="#" className="under-construction" onClick={closeMobileMenu}>
-                CAPACITY
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="#" className="under-construction" onClick={closeMobileMenu}>
+              <Link
+                to="#"
+                className="under-construction"
+                onClick={closeMobileMenu}
+              >
                 ANNOUNCEMENTS
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/profile" className="nav-links" onClick={closeMobileMenu}>
+              <Link
+                to="/profile"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 PROFILE
               </Link>
             </li>
+            <li className="nav-item">
+              <Link to="/">
+                <button className="signoutButton" onClick={logOff} href="/">
+                  SIGN OUT
+                </button>
+              </Link>
+            </li>
           </ul>
-
         </div>
       </nav>
     </>
